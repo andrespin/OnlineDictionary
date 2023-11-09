022b4b67c47240a83c42112cb4584e9ec6af2c81
Andrew <andrespin@mail.ru>
2023-11-09 23:59:38 +0300

    Set Graddle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,26 +37,51 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation(libs.versions.ktx)
+    implementation(libs.versions.appcompat)
+    implementation(libs.versions.material)
+    implementation(libs.versions.constraintlayout)
+    testImplementation(libs.versions.junit)
+    androidTestImplementation(libs.versions.androidxjunit)
+    androidTestImplementation(libs.versions.espressocore)
+
+    // Hilt
+//    implementation (libs.versions.hiltandroid)
+//    implementation (libs.versions.hiltcompiler)
+
+    // Hilt
+    implementation(libs.hilt.android.core)
+    implementation(libs.androidx.hilt.navigation.compose)
+    kapt(libs.hilt.compiler)
+
+
+
+//    implementation("com.google.android.material:material:1.10.0")
+//    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+//    testImplementation("junit:junit:4.13.2")
+//    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+//    material = "com.google.android.material:material:1.10.0"
+//    constraintlayout = "androidx.constraintlayout:constraintlayout:2.1.4"
+//    junit = "junit:junit:4.13.2"
+//    androidx_junit = "androidx.test.ext:junit:1.1.5"
+//    espresso-core = "androidx.test.espresso:espresso-core:3.5.1"
 
     // Hilt
 //    implementation ("com.google.dagger:hilt-android:2.48.1")
 //    implementation ("com.google.dagger:hilt-compiler:2.48.1")
 
     // Modules
-    implementation(project(":presentation"))
-    implementation(project(":presentation:about_app"))
-    implementation(project(":presentation:dictionary"))
-    implementation(project(":presentation:settings"))
-    implementation(project(":domain"))
-    implementation(project(":data"))
-    implementation(project(":data:local"))
-    implementation(project(":data:remote"))
+//    implementation(project(":presentation"))
+//    implementation(project(":presentation:about_app"))
+//    implementation(project(":presentation:dictionary"))
+//    implementation(project(":presentation:settings"))
+//    implementation(project(":domain"))
+//    implementation(project(":data"))
+//    implementation(project(":data:local"))
+//    implementation(project(":data:remote"))
 
 }
--- app/src/main/java/andrespin/onlinedictionary/MainActivity.kt
+++ app/src/main/java/andrespin/onlinedictionary/MainActivity.kt
@@ -1,6 +1,6 @@
 package andrespin.onlinedictionary
 
-import andrespin.domain.entity.Word
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,7 +8,6 @@ import android.os.Bundle
 //@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    lateinit var w: Word
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
