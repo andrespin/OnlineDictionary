54552c9e41ed8e4f37b35054118267781be4c48d
andrespin <andrespin@mail.ru>
2024-07-10 15:30:37 +0300

    Fix

--- data/remote/src/main/java/andrespin/remote/networking/WordApiService.kt
+++ data/remote/src/main/java/andrespin/remote/networking/WordApiService.kt
@@ -11,20 +11,4 @@ interface WordApiService {
         @Query("lang") lang: String,
         @Query("text") text: String,
     ) : Response<WordApiModel>
-
-
 }
-// dicservice.json/lookup?
-
-// https://dictionary.yandex.net/api/v1/dicservice.json/lookup?
-
-// https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=API-ключ&lang=en-ru&text=time
-
-
-
-//  "https://dictionary.yandex.net/api/v1/dicservice.json/lookup?"
-
-// https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=API-ключ&lang=en-ru&text=time
-// "dict.1.1.20240106T215927Z.3047be9fc89e7563.bbe6fdeccdf9fb3c49abcd0425acb617b9dc5c3b"
-
-// https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20240106T215927Z.3047be9fc89e7563.bbe6fdeccdf9fb3c49abcd0425acb617b9dc5c3b&lang=en-ru&text=time
